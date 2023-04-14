9a939b3ed4f7821f7521c393cec184c2a6b75494
Purushotam <55340065+MrPurushotam@users.noreply.github.com>
2023-04-14 21:45:43 +0530

    do updated

--- do.js
+++ do.js
@@ -1,23 +1,24 @@
 // name=document.querySelector(".name")
+let xyz=document.querySelector(".about-us")
 nameArray=["Purshotam ","Mritunjay","Muskan"]
 sectionArray="Koc-DW"
-imageArray={girl:"./IMAGE AND VIDEO/OIP.jpeg",boy:"./IMAGE AND VIDEO/office-Animated-Businessman-PNG-HD.png"}
+imageArray=["./IMAGE AND VIDEO/office-Animated-Businessman-PNG-HD.png","./IMAGE AND VIDEO/office-Animated-Businessman-PNG-HD.png","./IMAGE AND VIDEO/OIP.jpeg"]
 
 for(let i=0;i<3;i++){
     let image=document.createElement("span")
     image.setAttribute("class",`image image${i}`)
-    document.querySelector(".about-us").appendChild(image) 
-    image1=document.querySelector(`.image${i}`).createElement("img")
-    image1.setAttribute("src",`${imageArray.girl}`)
-    document.setAttribute(`.image${i}`).appendChild(image1)
+    xyz.appendChild(image) 
+    let image1=document.createElement("img")
+    image1.setAttribute("src",`${imageArray[i]}`)
+    document.querySelector(`.image${i}`).appendChild(image1)
 
     let name1=document.createElement("span")
     name1.setAttribute("class",`name name${i}`)
     name1.textContent=nameArray[i];
-    document.querySelector(".about-us").appendChild(name1)
+    xyz.appendChild(name1)
 
     let sec=document.createElement("span")
     sec.setAttribute("class",`section section${i}`)
     sec.textContent=sectionArray
-    document.querySelector(".about-us").appendChild(sec)
+    xyz.appendChild(sec)
 }
